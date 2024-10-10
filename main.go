3a0e60377313adbbb537f25c0deebebd6288eeac
alex <a.vitjukovs@gmail.com>
2024-10-10 16:20:59 +0300

    fix the source language article's URL

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -61,23 +60,18 @@ func main() {
 	}
 }
 
-func makeUrl(lang, query string) string {
-	url, err := url.Parse("https://" + lang + ".wikipedia.org/w/api.php?action=query&format=json&list=search&srlimit=1")
-	if err != nil {
-		panic("failed to parse URL")
-	}
+// Finds the matching article and returns its title and URL.
+// Uses opensearch API: https://www.mediawiki.org/wiki/API:Opensearch.
+//
+// Response is in the idiotic format of `[ string | []string ]`. Got the idea
+// for how to parse it from https://gist.github.com/crgimenes/c3b8b4fcce8529e9201f83c8da134f32.
+func findTitle(lang, query string) (title, titleUrl string, err error) {
+	reqUrl := Must(url.Parse("https://" + lang + ".wikipedia.org/w/api.php?action=opensearch&format=json&redirects=resolve&limit=1"))
+	q := reqUrl.Query()
+	q.Set("search", query)
+	reqUrl.RawQuery = q.Encode()
 
-	q := url.Query()
-	q.Set("srsearch", query)
-	url.RawQuery = q.Encode()
-
-	return url.String()
-}
-
-func findTitle(lang, query string) (title, url string, err error) {
-	url = makeUrl(lang, query)
-
-	resp, err := http.Get(url)
+	resp, err := http.Get(reqUrl.String())
 	if err != nil {
 		return "", "", err
 	}
@@ -88,23 +82,42 @@ func findTitle(lang, query string) (title, url string, err error) {
 		return "", "", err
 	}
 
-	var search struct {
-		Query struct {
-			Searches []struct {
-				Title string `json:"title"`
-			} `json:"search"`
-		} `json:"query"`
-	}
-	err = json.Unmarshal(respBytes, &search)
+	// parsing response like:
+	// ["aardvark",["Aardvark"],[""],["https://en.wikipedia.org/wiki/Aardvark"]]
+	var idioticApi []any
+	err = json.Unmarshal(respBytes, &idioticApi)
 	if err != nil {
 		return "", "", err
 	}
-
-	if len(search.Query.Searches) < 1 {
-		return "", "", fmt.Errorf("No results for %s\n", query)
+	if len(idioticApi) != 4 {
+		return "", "", fmt.Errorf("Expected 4 elements. Got: %#v", idioticApi)
 	}
 
-	return search.Query.Searches[0].Title, url, nil
+	titles, ok := idioticApi[1].([]any)
+	if !ok {
+		return "", "", fmt.Errorf("Expected a []any at [1]. Got: %#v", idioticApi[1])
+	}
+	if len(titles) == 0 {
+		return "", "", fmt.Errorf(`No results for "%s"`, title)
+	}
+	title, ok = titles[0].(string)
+	if !ok {
+		return "", "", fmt.Errorf("Expected a string at [1][0]. Got: %#v", titles[0])
+	}
+
+	urls, ok := idioticApi[3].([]any)
+	if !ok {
+		return "", "", fmt.Errorf("Expected a []any at [3]. Got: %#v", idioticApi[3])
+	}
+	if len(urls) == 0 {
+		return "", "", fmt.Errorf(`No results for "%s"`, title)
+	}
+	titleUrl, ok = urls[0].(string)
+	if !ok {
+		return "", "", fmt.Errorf("Expected a string at [3][0]. Got: %#v", urls[0])
+	}
+
+	return title, titleUrl, nil
 }
 
 type LangLink struct {
@@ -150,10 +163,17 @@ func getLangLinks(lang, title string) (langLinks []LangLink, err error) {
 	for _, v := range langs.Query.Pages {
 		return v.LangLinks, nil
 	}
-	return nil, errors.New("No results")
+	return nil, fmt.Errorf(`No results for "%s"`, title)
 }
 
 func exitUsage() {
 	fmt.Println(`Usage: wiki-translate [from=es] multi-word search term`)
 	os.Exit(0)
+}
+
+func Must[V any](value V, err error) V {
+	if err != nil {
+		panic(err)
+	}
+	return value
 }
